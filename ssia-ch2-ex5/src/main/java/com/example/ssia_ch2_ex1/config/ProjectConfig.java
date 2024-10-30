49aff2e12a05f67c8b939bf7ce2b17380760a287
bion0625 <bion0625@gmail.com>
2024-10-30 17:40:24 +0900

    2.3.5 프로젝트에 여러 구성 클래스 이용

---
+++ ssia-ch2-ex5/src/main/java/com/example/ssia_ch2_ex1/config/ProjectConfig.java
@@ -0,0 +1,29 @@
+package com.example.ssia_ch2_ex1.config;
+
+import com.example.ssia_ch2_ex1.security.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAuthenticationProvider authenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
