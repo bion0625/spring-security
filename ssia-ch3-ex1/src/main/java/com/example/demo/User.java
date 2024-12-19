a7629cba12e2c09308cb14f11abafefd022f534b
bion0625 <bion0625@gmail.com>
2024-12-19 17:37:28 +0900

    3.3.2 UserDetailsService 계약 구현

--- ssia-ch3-ex1/src/main/java/com/example/demo/User.java
+++ ssia-ch3-ex1/src/main/java/com/example/demo/User.java
@@ -1,16 +1,35 @@
 package com.example.demo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Data;
+import java.util.Collection;
+import java.util.List;
 
-@Entity
-@Data
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-	@Id
-	private Long id;
+public class User implements UserDetails {
+
 	private String username;
 	private String password;
 	private String authority;
+
+	public User(String username, String password, String authority) {
+		this.username = username;
+		this.password = password;
+		this.authority = authority;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(() -> this.authority);
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
 }
