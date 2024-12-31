a97e73cf1a5a41cc607123a650c20ece7cac3d96
bion0625 <bion0625@gmail.com>
2024-12-31 12:58:37 +0900

    13.5 승인 코드 그랜트 유형 이용

--- ssia-ch13-ex1/src/main/java/com/example/demo/ProjectConfig.java
+++ ssia-ch13-ex1/src/main/java/com/example/demo/ProjectConfig.java
@@ -2,14 +2,32 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
+	
+	@Bean
+	@Order(2)
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.httpBasic(Customizer.withDefaults());
+		http
+		.authorizeHttpRequests((authorize) -> authorize
+			.anyRequest().authenticated()
+		)
+		// Form login handles the redirect to the login page from the
+		// authorization server filter chain
+		.formLogin(Customizer.withDefaults());
+		return http.build();
+	}
 
 	@Bean
 	UserDetailsService userDetailsService() {
