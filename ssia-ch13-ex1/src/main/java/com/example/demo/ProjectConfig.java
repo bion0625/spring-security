ea0f61df252fe590f735b04d2d78509e1471b29e
bion0625 <bion0625@gmail.com>
2024-12-31 12:03:53 +0900

    13.2 사용자 관리 정의

---
+++ ssia-ch13-ex1/src/main/java/com/example/demo/ProjectConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		var service = new InMemoryUserDetailsManager();
+		var john = User.withUsername("john").password("12345").authorities("read").build();
+		service.createUser(john);
+		return service;
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
